35e3f6d888373d3ee5f4cb92cd542127334b609e
infradev1 <carloslarafp@gmail.com>
2025-05-08 10:52:41 -0700

    gRPC service test

--- internal/file/service_test.go
+++ internal/file/service_test.go
@@ -15,8 +15,14 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
+
+var address = make(chan string)
+
+const chunkSize = 5 * 1024 // 5 KB
 
 func TestUploadFile_Success(t *testing.T) {
 	ctx, cancel := signal.NotifyContext(context.Background(),
@@ -24,44 +30,25 @@ func TestUploadFile_Success(t *testing.T) {
 	)
 	defer cancel()
 
-	addr := make(chan string)
-
-	go server.NewFileServer(":0", NewService()).Start(ctx, addr)
+	go server.NewFileServer(":0", NewService()).Start(ctx, address)
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second*20)
 	defer cancel()
 
-	conn, err := grpc.NewClient(<-addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(<-address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
 	client := proto.NewFileManagerClient(conn)
-	fn := upload(ctx, client)
-	assert.NotEmpty(t, fn)
-}
-
-func upload(ctx context.Context, client proto.FileManagerClient) string {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	path := filepath.Join(dir, "../../", "gopher.png")
-
-	data, err := os.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	stream, err := client.UploadFile(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	chunkSize := 5 * 1024 // 5 KB
-	chunks := internal.SplitIntoChunks(data, chunkSize)
+	chunks := internal.SplitIntoChunks(loadData(), chunkSize)
 
 	for _, chunk := range chunks {
 		if err := stream.Send(&proto.UploadRequest{Chunk: chunk}); err != nil {
@@ -74,5 +61,56 @@ func upload(ctx context.Context, client proto.FileManagerClient) string {
 		log.Fatal(err)
 	}
 
-	return res.GetFileName()
+	assert.NotEmpty(t, res.GetFileName())
+}
+
+func TestUploadFile_Failure(t *testing.T) {
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM,
+	)
+	defer cancel()
+
+	go server.NewFileServer(":0", NewService()).Start(ctx, address)
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+
+	conn, err := grpc.NewClient(<-address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := proto.NewFileManagerClient(conn)
+
+	stream, err := client.UploadFile(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	empty := make([]byte, 0)
+	_ = stream.Send(&proto.UploadRequest{Chunk: empty})
+
+	_, err = stream.CloseAndRecv()
+	if err != nil {
+		assert.ErrorIs(t, err, status.Error(codes.InvalidArgument, "file chunk size must be greater than 0"))
+	} else {
+		assert.Fail(t, "expected error response from server due to empty chunk")
+	}
+}
+
+func loadData() []byte {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := filepath.Join(dir, "../../", "gopher.png")
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
 }
