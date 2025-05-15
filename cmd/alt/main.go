06c4fd39137de9f5fffaae647b49301ba2f0db04
infradev1 <carloslarafp@gmail.com>
2025-05-15 06:36:55 -0700

    added CLI output format option

--- cmd/alt/main.go
+++ cmd/alt/main.go
@@ -5,6 +5,7 @@ import (
 	"cmp"
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -49,9 +50,34 @@ type LogSummary struct {
 	types StatusCounts
 }
 
+func writeFile(contents []*LogSummary) error {
+	out, err := os.Create("out.txt")
+	if err != nil {
+		return fmt.Errorf("error creating output file: %w", err)
+	}
+	defer out.Close()
+
+	w := bufio.NewWriter(out)
+	defer w.Flush()
+
+	total := 0
+	for _, s := range contents {
+		_, err := fmt.Fprintf(w, "%s: %d total (INFO=%d, ERROR=%d, WARN=%d)\n",
+			s.node, s.total, s.types["[INFO]"], s.types["[ERROR]"], s.types["[WARN]"],
+		)
+		if err != nil {
+			return fmt.Errorf("error writing output: %w", err)
+		}
+		total += s.total
+	}
+	log.Printf("%d total log entries processed", total)
+
+	return nil
+}
+
 // gpu-node-17: 5 total (INFO=3, ERROR=1, WARN=1)
 // gpu-node-32: 2 total (INFO=1, ERROR=1, WARN=0)
-func (l *Log) PrintSummary(sortBy string) {
+func (l *Log) WriteSummary(sortBy string, outFormat string) error {
 	summary := make([]*LogSummary, 0)
 
 	for node, status := range l.nodeInfo {
@@ -74,6 +100,10 @@ func (l *Log) PrintSummary(sortBy string) {
 		return cmp.Compare(a.node, b.node) // descending order
 	})
 
+	if outFormat == "file" {
+		return writeFile(summary)
+	}
+
 	total := 0
 	for _, s := range summary {
 		log.Printf("%s: %d total (INFO=%d, ERROR=%d, WARN=%d)",
@@ -83,11 +113,13 @@ func (l *Log) PrintSummary(sortBy string) {
 	}
 
 	log.Printf("%d total log entries processed", total)
+
+	return nil
 }
 
 func main() {
-	if len(os.Args) != 4 {
-		log.Fatal("required format: go run main.go log.txt n sortBy, where n is max concurrency and sortBy: name | total")
+	if len(os.Args) != 5 {
+		log.Fatal("required format: go run main.go log.txt n sortBy, where n is max concurrency, sortBy: name | total, and output: print | file")
 	}
 	// read input file stream (logs.txt)
 	file, err := os.Open(os.Args[1])
@@ -102,6 +134,7 @@ func main() {
 	}
 
 	sortBy := os.Args[3]
+	outputFormat := os.Args[4]
 
 	nodeLog := NewLog()
 
@@ -139,5 +172,5 @@ func main() {
 		log.Printf("error parsing entry: %v", err)
 	}
 
-	nodeLog.PrintSummary(sortBy)
+	nodeLog.WriteSummary(sortBy, outputFormat)
 }
